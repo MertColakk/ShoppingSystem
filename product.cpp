cd3c5ca9f193f3b69ec5ed40c1bcd74644d1c6be
Mustafa Mert Çolak <meertcolak06@gmail.com>
2023-12-14 19:03:29 +0300

    Product Header File Function Content.

---
+++ product.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "product.hpp"
+
+void product::setID(int newID){this->id = newID;}
+
+void product::setName(std::string newName){this->name = newName;}
+
+void product::setPrice(float newPrice){this->price = newPrice;}
